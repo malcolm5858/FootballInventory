0a2ed2e074af5d1dd8fdc0f45c0f04948902783e
Malcolm Machesky <malcolmmachesky@Malcolms-MacBook-Pro.local>
2017-01-19 15:44:24 -0500

    Initial Commit

---
+++ FootballInventory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootballInventory
+//
+//  Created by Malcolm Machesky on 1/19/17.
+//  Copyright © 2017 Malcolm Machesky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
